cadd041a7c01261d503382fba86844bde5d0d352
Zmmmma <2451712879@qq.com>
2020-03-19 16:14:06 +0800

    Max

--- stat.js
+++ stat.js
@@ -1,8 +1,19 @@
+(function(){
+
+
+function reduce(array,combine,start){ 
+    var current=start; 
+    for(var element of array){ 
+    current=combine(current,element); 
+    } 
+    return current; 
+    } 
 var max=function(data){
     return reduce(
-        data, 
-        function(a,b){
+        data,function(a,b){
             return a>b.credit?a:b.credit;
             }
-        , 10)
+        , 0)
 }
+window.my={"max":max};
+})()
